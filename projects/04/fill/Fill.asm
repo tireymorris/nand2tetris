6f2a43ce11d740e6a74535aec2e99034e0f60a8a
Ty Morris <tireymorris@gmail.com>
2020-02-15 21:53:05 -0600

    Add project 4 - fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(BEGIN)
+// set fill bit to 1 (black) by default
+@1
+D=A
+@fill
+M=D
+
+// read keyboard value and decide whether to blacken or clear
+@KBD
+D=M;
+@BLACKEN
+D;JGT
+
+  (CLEAR)
+  // set fill bit to 0 if clear
+  @0
+  D=A
+  @fill
+  M=D
+
+  (BLACKEN)
+  // start pixel_idx at @SCREEN
+  @SCREEN
+  D=A
+  @pixel_idx 
+  M=D
+
+    (FILL_LOOP)
+    // fill next 16 bits with 1 (black)
+    @0
+    D=A
+    @fill
+    D=D-M // gets us -1 if black, else 0 if white
+    @pixel_idx
+    A=M // A holds address of next group of pixels to modify
+    M=D
+
+    // increment pixel_idx by 16
+    @1
+    D=A
+    @pixel_idx
+    M=D+M
+
+
+    // if we've not reached KBD (if KBD (24576) - idx > 0), JMP to FILL_LOOP
+    @KBD
+    D=A
+    @pixel_idx
+    D=D-M
+    @FILL_LOOP
+    D;JGT
+
+@BEGIN
+0;JMP
